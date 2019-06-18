334c09fbbb9bc77bb6bcf2d74fba8ab21c182513
linx9581 <linx9581@gmail.com>
2019-06-18 09:27:58 +0800

    add smile

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -9,7 +9,10 @@ import {
   ViroText,
   ViroConstants,  
   ViroBox,
-  ViroMaterials
+  ViroMaterials,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
@@ -32,6 +35,17 @@ export default class HelloWorldSceneAR extends Component {
         <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
         <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} />
         
+        <ViroAmbientLight color={"#aaaaaa"} />
+        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
+          position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
+        <Viro3DObject
+            source={require('./res/emoji_smile/emoji_smile.vrx')}
+            resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
+                require('./res/emoji_smile/emoji_smile_normal.png'),
+                require('./res/emoji_smile/emoji_smile_specular.png')]}
+            position={[-.5, .5, -1]}
+            scale={[.2, .2, .2]}
+            type="VRX" />
       </ViroARScene>
     );
   }
@@ -62,5 +76,6 @@ ViroMaterials.createMaterials({
     diffuseTexture: require('./res/grid_bg.jpg'),
   },
 });
+
 
 module.exports = HelloWorldSceneAR;
